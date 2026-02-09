10f9232eaa8619b8b81d592954adde55758aa346
Andrea Franceschetti <andrea.franceschetti@franka.de>
2026-02-09 16:35:07 +0000

    fix: probable use after free in PTP motion handler
    fix: keep shared ptrs alive in the threads
    
    fix: exit when abort, clean futures before resetting the control handler ptr
    
    fix: correct destructor order

--- franka_hardware/test/ptp_motion_test.cpp
+++ franka_hardware/test/ptp_motion_test.cpp
@@ -101,7 +101,7 @@ class PTPMotionTests : public ::testing::Test {
     EXPECT_CALL(*mock_robot, getCurrentState())
         .WillRepeatedly(::testing::ReturnRef(default_robot_state));
 
-    auto counter = 0;
+    size_t counter = 0;
     while (counter < kMaxCounter) {
       auto target_feedback = ptp_motion_handler->getFeedback(current_motion_id);
       if (target_feedback.status == franka::TargetStatus::kExecuting) {
@@ -121,7 +121,7 @@ class PTPMotionTests : public ::testing::Test {
     EXPECT_CALL(*mock_robot, getCurrentState())
         .WillRepeatedly(::testing::ReturnRef(default_robot_state));
 
-    auto counter = 0;
+    size_t counter = 0;
     while (counter < kMaxCounter) {
       auto target_feedback = ptp_motion_handler->getFeedback(current_motion_id);
       if (target_feedback.status == franka::TargetStatus::kTargetReached) {
